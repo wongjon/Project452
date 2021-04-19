e09e98dbfeb591987d2454caea0d6aa8c26d90b0
steptlee <steptlee@umich.edu>
2021-04-19 12:25:11 -0400

    passthrough

---
+++ filters/passthrough_filt.ino
@@ -0,0 +1,13 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputI2S            i2s1;           //xy=413,275
+AudioOutputI2S           i2s2;           //xy=648,277
+AudioConnection          patchCord1(i2s1, 0, i2s2, 0);
+AudioConnection          patchCord2(i2s1, 1, i2s2, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=551,396
+// GUItool: end automatically generated code
