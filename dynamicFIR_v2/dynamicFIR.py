7ef0fe830384aa50d7776be59104d1208c621f09
wongjon <wongjon@umich.edu>
2021-04-19 14:33:13 -0400

    coefficient float testing

--- dynamicFIR_v2/dynamicFIR.py
+++ dynamicFIR_v2/dynamicFIR.py
@@ -5,18 +5,37 @@ import numpy as np
 
 # Goal: Input 2 wav files and output a matrix of coefficients
 
-# Teceived tone
-fsR, received = wavfile.read("received.wav")
+# Received tone
+fsR, received = wavfile.read("center_received.wav")
 receivedF = fft(received)
+received_init = receivedF
+
 
 # Test tone
-fsT, test = wavfile.read("test.wav")
+fsT, test = wavfile.read("center.wav")
 testF = fft(test)
 
 # fsR and fsT should be equal to prevent sampling frequency discrepencies
 if fsT != fsR:
     print("received.wav and test.wav do not have the same sampling rate")
     exit()
+
+# ENSURE LENGTH OF TEST AND RECEIVED ARE SAME
+if len(received_init) == len(testF): #same size, no change
+    print("Same size")
+    receivedF = received_init
+    
+
+elif len(received_init) < len(testF): #rec is smaller, concat first elems onto beginning of rec
+    print("received smaller than test")
+    diff = len(testF) - len(received_init)
+    receivedF = np.concatenate((testF[0:(diff)], received_init))
+
+elif len(received_init) > len(testF): #rec is larger, remove last elem, concat first elems
+    print("received larger than test")
+    diff = len(received_init) - len(testF)
+    copy = received_init[0:(len(received_init) - (2*diff))]
+    receivedF = np.concatenate((testF[0:(diff)], copy))
 
 # Dynamic Filter h(t) = IFFT{Y(S)/X(S)}
 filterF = testF/receivedF
